5a3724f8ea42d621988a3caa31ff6a8d74e92003
Sara <147795804+naor4n@users.noreply.github.com>
2024-02-20 13:59:24 +0000

    counter + css

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,9 +1,11 @@
 const bananaImage = document.getElementById('bananaImage')
+const clickCountElement = document.getElementById('clickNumber')
 let bananas = 0
 
 bananaImage.addEventListener('click', () => {
     // Increment the cookie count
     bananas++
     // Update the display or send a request to the server
+    clickCountElement.textContent = bananas
     console.log('Banans:', bananas)
 })
